e2a38ff2bd1bd4b31dc8d2112b6172912ffd930b
santiagopalomino02-lang <santiago.palomino02@uceva.edu.co>
2026-07-31 18:05:10 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({ plugins: [react()] })
 export default defineConfig({
   base: '/ieso-sanza/',
-  // ...resto de tu configuración
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/ieso-sanza/',
 })
